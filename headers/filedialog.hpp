84609991215b0c0e7202f31c68355785dcd703c5
unknown989 <55900066+unknown989@users.noreply.github.com>
2021-09-01 14:31:34 -0700

    Update filedialog.hpp

--- headers/filedialog.hpp
+++ headers/filedialog.hpp
@@ -24,7 +24,56 @@ const char* openfilename(){
 	return filename;
 	
 }
+#elif defined(_WIN32) || defined(WIN32) 
+
+#if defined(UNICODE) && !defined(_UNICODE)
+    #define _UNICODE
+#elif defined(_UNICODE) && !defined(UNICODE)
+    #define UNICODE
+#endif
+#include <windows.h>
+#include <iostream>
+#include <commdlg.h>
+#include <winuser.h>
+#include <stdlib.h>
+// Code taken from https://github.com/unknown989/WChanger/ another project of mine , check it out
+const wchar_t *GetWC(const char *c)
+{
+    const size_t cSize = strlen(c)+1;
+    wchar_t* wc = new wchar_t[cSize];
+    mbstowcs (wc, c, cSize);
+
+    return wc;
+}
+bool openBox(LPCTSTR lpText,LPCTSTR lpCaption){
+    return (MessageBox(NULL,lpText,lpCaption,MB_OK)) ? true: false;
+}
+
+const char* openfilename(){
+	OPENFILENAME ofn = {0};
+    TCHAR szFile[260]={0};
+    ofn.lStructSize = sizeof(ofn);
+    ofn.lpstrFile = szFile;
+    ofn.nMaxFile = sizeof(szFile);
+    ofn.lpstrFilter =  _T("*.JPEG*;*.JPG*;*.PNG*;*.BMP*");
+    ofn.nFilterIndex = 0;
+    ofn.lpstrTitle = GetWC("Choose an image file...");
+    ofn.nFilterIndex = 1;
+    ofn.lpstrFileTitle = NULL;
+    ofn.nMaxFileTitle = 0;
+    ofn.lpstrInitialDir = NULL;
+    ofn.Flags = OFN_PATHMUSTEXIST | OFN_FILEMUSTEXIST;
+
+    if(!GetOpenFileName(&ofn)){
+        openBox(GetWC("Failed to open the image"),GetWC("Failure"));exit(1);
+    }
+	char* sfe = (char*)malloc(260);
+	wcstombs(sfe,szFile,260);
+	
+	return sfe;
+    
+}
 
 #else
-	printf("Platform is not recognized , if you have a mac OSX or Windows they are not supported YET!\n");
+	printf("Platform is not recognized , if you have a mac OSX it is not supported YET!\n");
 #endif
